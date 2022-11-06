8fd4c26c76e531fa9c1a020ca7fe8e582d1ec9c2
Balazs Keszei <kebalazs95@gmail.com>
2022-11-06 19:19:52 +0100

    new version 2022. nov. 6., vasárnap, 19:19:52 CET

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Régebben mindig is próbálkoztam saját blogot írni, több kevesebb sikerrel. Most egy újjabb próbálkozást láttok, ami lehet, hogy dugába dől mint az előzőek is. Próbálkoztam twitterrel, blog.hu-val, saját bloggal stb. Mivel web alkalmazás fejlesztő vagyok adott, hogy saját magamnak írjam meg a blogomat. A wordpress túl egyszerű választás lett volna....","categories": ["general"],
         "tags": ["jekyll","blog"],
         "url": "https://keszeitheone.github.io/general/initial-commit/"
+      },{
+        "title": "Mi az a TDD?",
+        "excerpt":"TDD, mint Test Driven Development. A jelenlegi munkahelyemen, ez egy fontos fejlesztési eszköz. Nagyon egyszerűen fogalmazva az alapelve, hogy először tesztet írunk és utána csak production kódot. A TDD fejlesztésnek 3 fő lépése van, ami folyamatosan körbe-körbe “hajtódik” végbe. Red Green RefactorRed lépés Először is mikor neki állunk egy algoritmus...","categories": ["general"],
+        "tags": ["develop","work","tdd"],
+        "url": "https://keszeitheone.github.io/general/mi-az-a-tdd/"
       },]
