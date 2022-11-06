1fd029765a3d09c92f2a333960a0ddb05a79972b
Balazs Keszei <kebalazs95@gmail.com>
2022-11-06 10:50:24 +0100

    new version 2022. nov. 6., vasárnap, 10:50:24 CET

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,11 @@ BRANCH=gh-pages
 BRANCH_FROM=$(git rev-parse --abbrev-ref HEAD)
 SRC=$(pwd)
 TEMP=$(mktemp -d)
+TEMP2=$(mktemp -d)
 trap "rm -rf ${TEMP}" EXIT
+trap "rm -rf ${TEMP2}" EXIT
+
+
 
 echo -e "\nBuilding Jekyll site:"
 rm -rf _dist
@@ -21,11 +25,14 @@ if [ ! -e _dist ]; then
   exit -1
 fi
 
+cp -R . ${TEMP}
+cd ${TEMP}
+
 cd _dist
 zip -r ../blog.dist.zip .
-cd ${SRC}
+cd ${TEMP}
 
-mv blog.dist.zip ${TEMP}
+mv blog.dist.zip ${TEMP2}
 
 echo -e "\nPreparing ${BRANCH} branch:"
 if [ -z "$(git branch -a | grep origin/${BRANCH})" ]; then
@@ -37,12 +44,14 @@ fi
 echo -e "\nDeploying into ${BRANCH} branch:"
 rm -rf *
 
-mv ${TEMP}/blog.dist.zip .
+mv ${TEMP2}/blog.dist.zip .
 unzip blog.dist.zip
-rm .gitignore
+rm -rf .gitignore
 rm -rf .idea
 rm blog.dist.zip
 
 git add .
 git commit -am "new version $(date)" --allow-empty
 git push origin ${BRANCH} 2>&1 | sed 's|'$URL'|[skipped]|g'
+
+cd ${SRC}
