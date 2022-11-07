ec111050b142e52c0d7625b048c820ec4945b772
Balazs Keszei <kebalazs95@gmail.com>
2022-11-07 22:52:55 +0100

    new version 2022. nov. 7., hétfő, 22:52:55 CET

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -13,4 +13,9 @@ var store = [{
         "excerpt":"TDD, mint Test Driven Development. A jelenlegi munkahelyemen, ez egy fontos fejlesztési eszköz. Nagyon egyszerűen fogalmazva az alapelve, hogy először tesztet írunk és utána csak production kódot. A TDD fejlesztésnek 3 fő lépése van, ami folyamatosan körbe-körbe “hajtódik” végbe. Red Green RefactorRed lépés Először is mikor neki állunk egy algoritmus...","categories": ["development"],
         "tags": ["develop","work","tdd"],
         "url": "https://keszeitheone.github.io/development/mi-az-a-tdd/"
+      },{
+        "title": "Lando, a web fejlesztők legjobb eszköze",
+        "excerpt":"Folyamatosan kutakodom új technológiák után ami érdekes és hasznos is egyben. Web fejlesztéshez szükséges fejlesztői eszközök tárháza szinte végtelen és nagyon nehéz megtalálni a megfelelőt ami a te fejlesztési folyamataidhoz hozzá kapcsolódik. Mindig zavart, az hogy különböző projektek miatt váltani kell php, adatbázis valamint nodejs verziókat. Rengeteg idő ment el...","categories": ["development"],
+        "tags": ["develop","work","lando","devops","tutorial"],
+        "url": "https://keszeitheone.github.io/development/lando-a-web-fejlesztk-legjobb-eszkoze/"
       },]
